0a00c73d356a484bb4874b2b5515b7a77b8f9756
thiago leite <thiagomoreiraleite@gmail.com>
2021-02-23 02:48:13 -0300

    footer done

--- app/javascript/packs/slides.js
+++ app/javascript/packs/slides.js
@@ -10,6 +10,10 @@ const changeSlide = () =>{
             background.className = 'one';
         } else {
             background.style.opacity = "0";
+            setTimeout(function(){ two.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ three.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ four.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ one.className = 'yellow'; }, 50);
             setTimeout(function(){ background.className = 'one'; }, 600);
             setTimeout(function(){ background.style.opacity = "1"; }, 600);
         }
@@ -20,6 +24,10 @@ const changeSlide = () =>{
             background.className = 'two';
         } else {
             background.style.opacity = "0";
+            setTimeout(function(){ one.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ three.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ four.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ two.className = 'yellow'; }, 50);
             setTimeout(function(){ background.className = 'two'; }, 600);
             setTimeout(function(){ background.style.opacity = "1"; }, 600);
         }
@@ -30,6 +38,10 @@ const changeSlide = () =>{
             background.className = 'three';
         } else {
             background.style.opacity = "0";
+            setTimeout(function(){ two.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ one.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ four.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ three.className = 'yellow'; }, 50);
             setTimeout(function(){ background.className = 'three'; }, 600);
             setTimeout(function(){ background.style.opacity = "1"; }, 600);
         }
@@ -40,6 +52,10 @@ const changeSlide = () =>{
             background.className = 'four';
         } else {
             background.style.opacity = "0";
+            setTimeout(function(){ two.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ three.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ one.className = 'tab-underlined'; }, 50);
+            setTimeout(function(){ four.className = 'yellow'; }, 50);
             setTimeout(function(){ background.className = 'four'; }, 600);
             setTimeout(function(){ background.style.opacity = "1"; }, 600);
         }
