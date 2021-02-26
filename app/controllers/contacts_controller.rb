1eaa8dd6ee885167c5ed18f51e0e33fecbcc4e2a
Malin Schadel <malin.schadel@gmail.com>
2021-02-26 21:24:05 +0100

    added form to home page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,16 +9,9 @@ class ContactsController < ApplicationController
     if @contact.deliver
       flash.now[:error] = nil
       redirect_to root_path, notice: 'Message sent successfully'
-      raise
     else
       flash.now[:error] = 'Cannot send message'
       render :new
     end
-  end
-
-  private
-
-  def contact_params
-    params.require(:contact).permit()
   end
 end
