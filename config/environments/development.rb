02b46ae44ebaba91e04d3da3e0eea10b97197d28
thiago leite <thiagomoreiraleite@gmail.com>
2021-03-02 15:00:34 -0300

    test contact form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # config.action_mailer.default_url_options = { host: 'https://agenciametanoia.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://agenciametanoia.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # config.action_mailer.default_url_options = { host: 'https://agenciametanoia.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://agenciametanoia.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
